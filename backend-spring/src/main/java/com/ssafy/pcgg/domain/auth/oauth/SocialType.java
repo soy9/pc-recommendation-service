1bedbda03410bed2a40c56eb2314dcfc252949cd
soy9 <67994488+soy9@users.noreply.github.com>
2023-11-27 15:28:32 +0900

    Delete backend-spring directory

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/auth/oauth/SocialType.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.pcgg.domain.auth.oauth;
-
-public enum SocialType {
-    KAKAO, NAVER, GOOGLE
-}
--- backend-spring/src/main/java/com/ssafy/pcgg/domain/recommend/dto/DesktopRecommendDto.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.pcgg.domain.recommend.dto;
-
-public class DesktopRecommendDto {
-    private String usage;
-    private Integer budget;
-    private boolean os;
-    private String priority;
-}
